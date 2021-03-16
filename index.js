f305580d3ad58aea1dfb78b48217acddee34db4e
dapeshcode <ampeccia@gmail.com>
2021-03-16 01:01:16 -0500

    commit tree 784227e98707a029e7589f81a5c924b7aaf7ad0b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+//dodger moves left
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+//move dodger right 
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
